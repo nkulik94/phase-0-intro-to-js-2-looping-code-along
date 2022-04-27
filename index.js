73959012bcc564866812f6b4dbfcb84b629c3333
Naftali Kulik <nkulik1994@gmail.com>
2022-04-27 10:58:49 -0400

    commit tree 47afc8630d1c0316dac66d4b4dd4d6801ea51c3e

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    const messages = []
+    for (let i =0; i < array.length; i++) {
+        const message = `Thank you, ${array[i]}, for the wonderful ${event} gift!`
+        messages[i] = message
+    }
+    return messages
+}
+function countDown(n) {
+    let i = 0
+    while (i <= n) {
+        console.log(n)
+        n--
+    }
+}
